d729f72c230201b4805eef48461342afbbc0b105
Brian Kloppenborg <brian@arrayfire.com>
2014-11-12 17:39:14 -0500

    Fix benchmark vs. baseline naming.

--- src/bilateral.cpp
+++ src/bilateral.cpp
@@ -13,7 +13,7 @@ using namespace af;
 extern unsigned int samples;
 extern unsigned int operations;
 
-BASELINE_F(BilateralFilter_f32, Benchmark, Fixture_2D_f32, samples, operations){}
+BASELINE_F(BilateralFilter_f32, Baseline, Fixture_2D_f32, samples, operations){}
 
 BENCHMARK_F(BilateralFilter_f32, Benchmark, Fixture_2D_f32, samples, operations)
 {
@@ -21,7 +21,7 @@ BENCHMARK_F(BilateralFilter_f32, Benchmark, Fixture_2D_f32, samples, operations)
 	B.eval();
 }
 
-BASELINE_F(BilateralFilter_f64, Benchmark, Fixture_2D_f64, samples, operations){}
+BASELINE_F(BilateralFilter_f64, Baseline, Fixture_2D_f64, samples, operations){}
 
 BENCHMARK_F(BilateralFilter_f64, Benchmark, Fixture_2D_f64, samples, operations)
 {
