d3a96b25af4762fdb66b4c7b050b2f64850efdf7
Brian Kloppenborg <brian@arrayfire.com>
2014-12-03 15:18:58 -0500

    Rescale histogram values to not fall in one bin.

--- src/histogram.cpp
+++ src/histogram.cpp
@@ -14,19 +14,35 @@ extern unsigned int samples;
 extern unsigned int operations;
 
 
-BASELINE_F(Histogram_f32, Baseline, Fixture_2D_f32, samples, operations){ }
+BASELINE_F(Histogram_f32, Baseline, Fixture_2D_f32, samples, operations)
+{
+	// rescale values to 0 -> 255 to correspond to bin ranges
+    A *= 255;
+    A.eval();
+}
 
 BENCHMARK_F(Histogram_f32, Benchmark, Fixture_2D_f32, samples, operations)
 {
+	// rescale values to 0 -> 255 to correspond to bin ranges
+    A *= 255;
+    A.eval();
 	array B = histogram(A, 256, 0, 255);
 	B.eval();
 }
 
 
-BASELINE_F(Histogram_f64, Baseline, Fixture_2D_f64, samples, operations){ }
+BASELINE_F(Histogram_f64, Baseline, Fixture_2D_f64, samples, operations)
+{
+	// rescale values to 0 -> 255 to correspond to bin ranges
+    A *= 255;
+    A.eval();
+}
 
 BENCHMARK_F(Histogram_f64, Benchmark, Fixture_2D_f64, samples, operations)
 {
+	// rescale values to 0 -> 255 to correspond to bin ranges
+    A *= 255;
+    A.eval();
 	array B = histogram(A, 256, 0, 255);
 	B.eval();
 }
