aad0f89d7a33d192cbd03fef457f1543e1e17a02
Brian Kloppenborg <bkloppenborg@gmail.com>
2015-06-18 17:38:46 -0400

    Fix pinned memory benchmark.

--- src/pinned.cpp
+++ src/pinned.cpp
@@ -16,52 +16,54 @@ extern unsigned int operations;
 class PinnedFixture_2D_f32 : public Fixture_2D_f32
 {
 public:
-	float * A_host;
-
-	PinnedFixture_2D_f32() {}
-
-	virtual void setUp(int64_t experimentSize)
-	{
+    float * A_host;
+    int64_t A_size;
+    
+    PinnedFixture_2D_f32() {}
+    
+    virtual void setUp(int64_t experimentSize)
+    {
+	A_size = experimentSize;
         A_host = af::pinned<float>(size_t(experimentSize));
-	}
-
-	virtual void tearDown()
-	{
+    }
+    
+    virtual void tearDown()
+    {
         af::freePinned(A_host);
-	}
+    }
 };
 
 class PinnedFixture_2D_f64 : public Fixture_2D_f64
 {
 public:
-	double * A_host;
-
-	PinnedFixture_2D_f64() {}
-
-	virtual void setUp(int64_t experimentSize)
-	{
+    double * A_host;
+    int64_t A_size;
+    
+    PinnedFixture_2D_f64() {}
+    
+    virtual void setUp(int64_t experimentSize)
+    {
+	A_size = experimentSize;
         A_host = af::pinned<double>(size_t(experimentSize));
-	}
-
-	virtual void tearDown()
-	{
+    }
+    
+    virtual void tearDown()
+    {
         af::freePinned(A_host);
-	}
+    }
 };
 
 BASELINE_F(PinnedMemory, Baseline, PinnedFixture_2D_f32, samples, operations) { }
 
 BENCHMARK_F(PinnedMemory, PinnedMemory_f32_Bandwidth, PinnedFixture_2D_f32, samples, operations)
 {
-    dim4 A_size = A.dims();
-    array B = array(A_size.dims[0], A_size.dims[1], A_host, afHost);
+    array B = array(A_size, A_host, afHost);
     B.eval();
 }
 
 BENCHMARK_F(PinnedMemory, PinnedMemory_f64_Bandwidth, PinnedFixture_2D_f64, samples, operations)
 {
-    dim4 A_size = A.dims();
-    array B = array(A_size.dims[0], A_size.dims[1], A_host, afHost);
+    array B = array(A_size, A_host, afHost);
     B.eval();
 }
 
