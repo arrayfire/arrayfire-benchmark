822ceada80535e3b6c4f1887f9eb6904df476405
Brian Kloppenborg <brian@kloppenborg.net>
2015-07-14 12:11:00 -0400

    Add missing cds

--- updateLibraries.sh
+++ updateLibraries.sh
@@ -6,13 +6,13 @@
 ROOT_DIR=~/benchmarking
 
 # Ensure we have a copy of ArrayFire on the system
+cd ${ROOT_DIR}
 if [ ! -d ${ROOT_DIR}/arrayfire ] ; then
     git clone https://github.com/arrayfire/arrayfire.git
-else
-    cd ${ROOT_DIR}/arrayfire
-    git pull
 fi
 # update ArrayFire and all submodules
+cd ${ROOT_DIR}/arrayfire
+git pull
 git submodule init
 git submodule update
 # Compile ArrayFire and install it to a local directory
@@ -23,13 +23,13 @@ make -j8
 make install
 
 # Ensure the benchmark suite exists on the local machine
+cd ${ROOT_DIR}
 if [ ! -d ${ROOT_DIR}/arrayfire_benchmark ] ; then
     git clone https://github.com/bkloppenborg/arrayfire_benchmark
-else
-    cd ${ROOT_DIR}/arrayfire_benchmark
-    git pull
 fi
 # update the benchmark suite
+cd ${ROOT_DIR}/arrayfire_benchmark
+git pull
 git submodule init
 git submodule update
 # compile it
