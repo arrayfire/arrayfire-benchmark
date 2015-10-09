d29c6ea74201c782764ec042f6c267a1fcc88379
Shehzan Mohammed <shehzan@arrayfire.com>
2015-10-09 19:42:21 -0400

    Add f64 to elwise benchmarks

--- src/elwise.cpp
+++ src/elwise.cpp
@@ -19,16 +19,33 @@ public:
     af_dtype data_type;
     array A;
     array B;
-    array C;
+    int max_pow;
 
-    AF_ELWISE_Fixture() { this->data_type = af_dtype::f32; }
+    AF_ELWISE_Fixture()
+    {
+        this->data_type = af_dtype::f32;
+        max_pow = 25;
+    }
+
+    AF_ELWISE_Fixture(af_dtype data_type)
+    {
+        this->data_type = data_type;
+        switch(this->data_type)
+        {
+            case f32: max_pow = 25; break;  // 32M *  4B = 128MB
+            case f64: max_pow = 24; break;  // 16M *  8B = 128MB
+            case c32: max_pow = 24; break;  // 16M *  8B = 128MB
+            case c64: max_pow = 23; break;  //  8M * 16B = 128MB
+            default : max_pow = 25; break;  // 32M *  4B = 128MB
+        }
+    }
 
     virtual std::vector<std::pair<int64_t, uint64_t>> getExperimentValues() const
     {
         std::vector<std::pair<int64_t, uint64_t>> problemSpace;
         // 256 - 1048576 elements (2^8 - 2^20)
         // 256 - 33554432 elements (2^8 - 2^25)
-        for(int i = 8; i <= 25; i++)
+        for(int i = 8; i <= max_pow; i++)
         {
             auto experiment_size = std::make_pair<int64_t, uint64_t>(pow(2, i), 0);
             problemSpace.push_back(experiment_size);
@@ -47,10 +64,8 @@ public:
             deviceGC();
             A = randu(experimentSize, this->data_type);
             B = randu(experimentSize, this->data_type);
-            C = randu(experimentSize, this->data_type);
             A.eval();
             B.eval();
-            C.eval();
             af::sync();
         }
         catch (af::exception & e)
@@ -63,67 +78,87 @@ public:
 
 };
 
-// do-nothing baseline measurement
-BASELINE_F( ELWISE, Baseline, AF_ELWISE_Fixture, samples,  operations) {}
+class AF_ELWISE_Fixture_f32 : public AF_ELWISE_Fixture
+{
+public:
+    AF_ELWISE_Fixture_f32() : AF_ELWISE_Fixture(af_dtype::f32) {}
+};
 
-#define ELWISE_BENCHMARK(functionName, operation , fixture ) \
-BENCHMARK_F( ELWISE , ELWISE_##functionName , fixture , samples, operations) \
-{   \
-    af::array result = operation ;  \
-    result.eval();                  \
-}   \
+class AF_ELWISE_Fixture_f64 : public AF_ELWISE_Fixture
+{
+public:
+    AF_ELWISE_Fixture_f64() : AF_ELWISE_Fixture(af_dtype::f64) {}
+};
+
+// do-nothing baseline measurement
+BASELINE_F( ELWISE_f32, Baseline, AF_ELWISE_Fixture_f32, samples,  operations) {}
+BASELINE_F( ELWISE_f64, Baseline, AF_ELWISE_Fixture_f64, samples,  operations) {}
+
+#define ELWISE_BENCHMARK(functionName, operation)                               \
+BENCHMARK_F( ELWISE_f32, ELWISE_f32_##functionName, AF_ELWISE_Fixture_f32,      \
+        samples, operations)                                                    \
+{                                                                               \
+    af::array result = operation ;                                              \
+    result.eval();                                                              \
+}                                                                               \
+BENCHMARK_F( ELWISE_f64, ELWISE_f64_##functionName, AF_ELWISE_Fixture_f64,      \
+        samples, operations)                                                    \
+{                                                                               \
+    af::array result = operation ;                                              \
+    result.eval();                                                              \
+}                                                                               \
 
 //
 // Benchmark ArrayFire functions individually:
 //
 
 // built-in functions
-ELWISE_BENCHMARK(MIN, af::min(A, B), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(MAX, af::max(A, B), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(ATAN2, af::atan2(A, B), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(HYPOT, af::hypot(A, B), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(POW, af::pow(A, B), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(REMAINDER, af::rem(A, B), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(MODULO, af::mod(A, B), AF_ELWISE_Fixture)
+ELWISE_BENCHMARK(MIN        , af::min(A, B))
+ELWISE_BENCHMARK(MAX        , af::max(A, B))
+ELWISE_BENCHMARK(ATAN2      , af::atan2(A, B))
+ELWISE_BENCHMARK(HYPOT      , af::hypot(A, B))
+ELWISE_BENCHMARK(POW        , af::pow(A, B))
+ELWISE_BENCHMARK(REMAINDER  , af::rem(A, B))
+ELWISE_BENCHMARK(MODULO     , af::mod(A, B))
 
 // trig functions
-ELWISE_BENCHMARK(SINE,     af::sin(A),    AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(COSINE,   af::cos(A),    AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(TANGENT,   af::tan(A),    AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(ARC_SINE,     af::asin(A),    AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(ARC_COSINE,   af::acos(A),    AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(ARC_TANGENT,   af::atan(A),    AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(HYPERBOLIC_SINE,     af::sinh(A),    AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(HYPERBOLIC_COSINE,   af::cosh(A),    AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(HYPERBOLIC_TANGENT,   af::tanh(A),    AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(HYPERBOLIC_ARC_SINE,     af::asinh(A),    AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(HYPERBOLIC_ARC_COSINE,   af::acosh(A),    AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(HYPERBOLIC_ARC_TANGENT,   af::atanh(A),    AF_ELWISE_Fixture)
+ELWISE_BENCHMARK(SIN        , af::sin(A))
+ELWISE_BENCHMARK(COS        , af::cos(A))
+ELWISE_BENCHMARK(TAN        , af::tan(A))
+ELWISE_BENCHMARK(ARC_SIN    , af::asin(A))
+ELWISE_BENCHMARK(ARC_COS    , af::acos(A))
+ELWISE_BENCHMARK(ARC_TAN    , af::atan(A))
+ELWISE_BENCHMARK(HYP_SIN    , af::sinh(A))
+ELWISE_BENCHMARK(HYP_COS    , af::cosh(A))
+ELWISE_BENCHMARK(HYP_TAN    , af::tanh(A))
+ELWISE_BENCHMARK(HYP_ARC_SIN, af::asinh(A))
+ELWISE_BENCHMARK(HYP_ARC_COS, af::acosh(A))
+ELWISE_BENCHMARK(HYP_ARC_TAN, af::atanh(A))
 
 // exponentals and logs
-ELWISE_BENCHMARK(EXP, af::exp(A), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(EXP_M1, af::expm1(A), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(ERF, af::erf(A), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(ERFC, af::erfc(A), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(LOG_E, af::log(A), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(LOG_1P, af::log1p(A), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(LOG10, af::log10(A), AF_ELWISE_Fixture)
+ELWISE_BENCHMARK(EXP        , af::exp(A))
+ELWISE_BENCHMARK(EXP_M1     , af::expm1(A))
+ELWISE_BENCHMARK(ERF        , af::erf(A))
+ELWISE_BENCHMARK(ERFC       , af::erfc(A))
+ELWISE_BENCHMARK(LOG_E      , af::log(A))
+ELWISE_BENCHMARK(LOG_1P     , af::log1p(A))
+ELWISE_BENCHMARK(LOG10      , af::log10(A))
 
 // other functions
-ELWISE_BENCHMARK(SQRT, af::sqrt(A), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(CBRT, af::cbrt(A), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(IS_ZERO, af::iszero(A), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(IS_INF, af::isInf(A), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(IS_NAN, af::isNaN(A), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(TGAMMA, af::tgamma(A), AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(LGAMMA, af::lgamma(A), AF_ELWISE_Fixture)
+ELWISE_BENCHMARK(SQRT       , af::sqrt(A))
+ELWISE_BENCHMARK(CBRT       , af::cbrt(A))
+ELWISE_BENCHMARK(IS_ZERO    , af::iszero(A))
+ELWISE_BENCHMARK(IS_INF     , af::isInf(A))
+ELWISE_BENCHMARK(IS_NAN     , af::isNaN(A))
+ELWISE_BENCHMARK(TGAMMA     , af::tgamma(A))
+ELWISE_BENCHMARK(LGAMMA     , af::lgamma(A))
 
 // basic mathematical operators
-ELWISE_BENCHMARK(ADD,      A + B,     AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(SUBTRACT, A - B,     AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(MULTIPLY, A * B,     AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(DIVIDE,   A / B,     AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(ADD_CONSTANT, A + 1, AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(SUBTRACT_CONSTANT, A - 1, AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(MULTIPY_CONSTANT, 2 * A, AF_ELWISE_Fixture)
-ELWISE_BENCHMARK(DIVIDE_CONSTANT, A / 2, AF_ELWISE_Fixture)
+ELWISE_BENCHMARK(ADD        , A + B)
+ELWISE_BENCHMARK(SUBTRACT   , A - B)
+ELWISE_BENCHMARK(MULTIPLY   , A * B)
+ELWISE_BENCHMARK(DIVIDE     , A / B)
+ELWISE_BENCHMARK(ADD_CONSTANT       , A + 1)
+ELWISE_BENCHMARK(SUBTRACT_CONSTANT  , A - 1)
+ELWISE_BENCHMARK(MULTIPY_CONSTANT   , 2 * A)
+ELWISE_BENCHMARK(DIVIDE_CONSTANT    , A / 2)
