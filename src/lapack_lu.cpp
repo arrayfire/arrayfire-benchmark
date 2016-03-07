ff041e86eee28e0b3631173186b91ae1657dc20a
Pavan Yalamanchili <pavan@arrayfire.com>
2016-03-07 17:52:54 -0500

    Fixing baseline benchmarks for LU

--- src/lapack_lu.cpp
+++ src/lapack_lu.cpp
@@ -14,15 +14,16 @@ extern unsigned int samples;
 extern unsigned int operations;
 
 
-BASELINE_F(LAPACK_LU, Baseline, Fixture_2D_f32, samples, operations) { }
+BASELINE_F(LAPACK_LU_f32, Baseline, Fixture_2D_f32, samples, operations) { }
+BASELINE_F(LAPACK_LU_f64, Baseline, Fixture_2D_f64, samples, operations) { }
 
-BENCHMARK_F(LAPACK_LU, LU_f32, Fixture_2D_f32, samples, operations)
+BENCHMARK_F(LAPACK_LU_f32, LU_f32, Fixture_2D_f32, samples, operations)
 {
     array pivot, l, u;
     lu(l, u, pivot, A);
 }
 
-BENCHMARK_F(LAPACK_LU, LU_f64, Fixture_2D_f64, samples, operations)
+BENCHMARK_F(LAPACK_LU_f64, LU_f64, Fixture_2D_f64, samples, operations)
 {
     array pivot, l, u;
     lu(l, u, pivot, A);
